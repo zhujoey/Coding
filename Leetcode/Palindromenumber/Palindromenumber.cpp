693aab220216d0b02f45d6a347b81453d70e4cf6
Joey <zhujoey@outlook.com>
2024-06-25 01:17:29 -0700

    palindrom
    finished problem

---
+++ Leetcode/Palindromenumber/Palindromenumber.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        std::string num = std::to_string(x);
+        for (int i = 0; i< num.size() / 2; ++i)
+        {
+            if (num[i] != num[num.size() - 1 - i])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
